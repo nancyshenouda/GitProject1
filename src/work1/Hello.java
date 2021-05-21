c08ce6961f04ccc1226c6de78de019d66e3bbeee
nancyshenouda <Shenouda.nancy@gmail.com>
2021-05-21 19:47:12 -0400

    created first repo

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -3,5 +3,6 @@ package work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello work1");
+        System.out.println("Created the first Repository");
     }
 }
